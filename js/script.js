3f54bf0b003c673b2dd3b56cbe318d935a45689d
Gianluca Quaranta <info@gianlucaquaranta.com>
2023-11-14 17:35:19 +0100

    stesura js - css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-{\rtf1}
+// seleziono btn da dom
+let btn = document.querySelector('.btnCeles');
+// seleziono checkbox da dom
+let nomeForm = document.querySelector('#cb');
+console.log(nomeForm);
+/* cbCheck()
+function cbCheck(){
+    if (nomeForm.cb.checked){
+        alert("CheckBox spuntato");
+    }
+    else{
+        alert("CheckBox non spuntato");
+    }
+}
+ */
+
+/* function abilitaBtn(){
+    if (checkbox.checked){
+        alert("CheckBox spuntato");
+    }
+    else{
+        alert("CheckBox non spuntato");
+    }
+}
+abilitaBtn() */
