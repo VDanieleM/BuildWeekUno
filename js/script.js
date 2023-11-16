dd8385524a4492056e85c930fe416ae450e99f9e
Gianluca Quaranta <info@gianlucaquaranta.com>
2023-11-16 17:43:00 +0100

    implementazione js css html

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-//Seleziono btn da dom
+//Sezione index
+
+//Seleziono btn da dom ed assegno a mia variabile
 let btn = document.querySelector('#btn');
-//Seleziono checkbox da dom
+//Seleziono checkbox da dom ed assegno a mia variabile
 let checkbox = document.querySelector('#cb');
 
 //Dichiaro funzione abilitaBtn()
 function abilitaBtn(){
+    
     //Setto il bottone come disable
     btn.disabled = true;
     //Setto un ascoltatore che al click della checkbox mi controlla (segue commento)
@@ -17,11 +20,110 @@ function abilitaBtn(){
     else{
         //Setto il bottone come attivo
         btn.disabled = false;
+        btn.addEventListener('click', () => {
+            window.location = 'benchmark.html'
+        })
     }
     })
 }
-//Richiamo la funzione abilitaBtn()
-abilitaBtn();
+
+// Verifico se la pagina corrente è quella desiderata
+if (window.location.href.indexOf("welcome.html") > -1) {
+    // Eseguo la funzione solo se la condizione è soddisfatta
+    //Richiamo la  funzione abilitaBtn()
+    abilitaBtn();
+}
+
+//Sezione benchmark
+
+ //Seleziono div timer da dom ed assegno a mia variabile
+let timer = document.querySelector('#timer-number');
+
+//Dichiaro variabile d'inizio countdown
+let count = 10;
+
+function startCountdown(){
+    //Inserisco il countdown legandolo alla mia variabile timer
+    timer.textContent = count;
+    //Imposto setInterval ed assegno a mia variabile contatore
+    let contatore = setInterval(function() {
+        //Inserisco il countdown legandolo alla mia variabile timer
+        timer.textContent = count;
+        //Avvio il countdown
+        count--
+        //Verifico che il valore di riferimento count è minore di 0
+        if (count < 0) {
+            //Pulisco il setInterval della variabile contatore
+            clearInterval(contatore);
+            
+            //Carico domande
+            /*
+            
+            funzione()
+            */
+        
+        }
+}, 1000);
+}
+startCountdown();
+
+    
+/* 
+    //Imposto setInterval ed assegno a mia variabile contatore
+    let contatore = setInterval(function(){
+        //Inserisco il countdown legandolo alla mia variabile timer
+        timer.textContent = count;
+        //Avvio il countdown
+        count--
+        //Verifico che il valore di riferimento count è minore di 0
+        if (count < 0) {
+            //Pulisco il setInterval della variabile contatore
+            clearInterval(contatore);
+            
+            //Carico domande
+            /*
+            
+            funzione()
+        
+        
+        }
+        //Dichiaro la velocità di countdown
+    }, 1000);
+};
+    
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function cambiostella() {
     const stelleAttive = document.querySelectorAll(".stellina");
